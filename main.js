d6bc2124c332048426b5c585d647b2403dbd08df
silverofw <silverofw@hotmail.com>
2019-11-11 19:03:32 +0800

    Update main.js

--- main.js
+++ main.js
@@ -8,13 +8,70 @@ function ShowData(list){
     var jo = data;
     document.getElementById("demo").innerHTML = "";
     for (var i = 0, len = jo.data.length; i < len; i ++) {
-      document.getElementById("demo").innerHTML += 
-      jo.data[i].id + 
-      " " + jo.data[i].card_type + 
-      " [ " + jo.data[i].name_cn + " ]</br>";
+
+      var cardBgCss = "\"effect_card_bg\"";
+      var cardBgCss1 = "\"effect_card_bg1\"";
+      var cardBgCss2 = "\"effect_card_bg2\"";
+      switch(jo.data[i].card_type)
+      {
+        case "效果怪獸":
+          cardBgCss = "\"effect_card_bg\"";
+          cardBgCss1 = "\"effect_card_bg1\"";
+          break;
+        case "通常魔法":
+        case "儀式魔法":
+        case "速攻魔法":
+        case "永續魔法":
+        case "裝備魔法":
+        case "場地魔法":
+          cardBgCss = "\"spell_card_bg\"";
+          cardBgCss1 = "\"spell_card_bg1\"";
+          break;
+        case "通常陷阱":
+        case "永續陷阱":
+        case "反擊陷阱":
+          cardBgCss = "\"trap_card_bg\"";
+          cardBgCss1 = "\"trap_card_bg1\"";
+          break;
+        default:
+          cardBgCss = "\"effect_card_bg\"";
+          cardBgCss1 = "\"effect_card_bg1\"";
+      }
+
 
       document.getElementById("demo").innerHTML += 
-      "     " + jo.data[i].card_text_cn + "</br></br>";
+        "<div id = \"card_bg\">" + 
+          "<div id =" + cardBgCss + ">" + 
+            jo.data[i].id + "&nbsp" +
+          " </div>" + 
+          "<div id=" + cardBgCss1 +">" + 
+            " [ " + jo.data[i].name_cn + " ]&nbsp" + 
+            jo.data[i].card_type + "</br>" +    
+            "<div id="+cardBgCss2+">" +
+              jo.data[i].card_text_cn + "</br>" + 
+            "</div>" + 
+          "</div>" + 
+        "</div>";
     }
   });
+}
+
+function ScrollTop()
+{
+  $('html,body').animate({scrollTop:0}, 333);
+}
+
+function OnOver(x)
+{
+  x.style.color = "red";
+}
+
+function OnOut(x)
+{
+  x.style.color = "black";
+}
+
+function OpenUrl(url)
+{
+  location.href = url;
 }
