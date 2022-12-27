1c7cd7a1d7bb14216770e831765ef2a571b67fc2
Silver <silverofw@gmail.com>
2022-12-28 01:13:07 +0800

    [UPDATE] add article

--- main.js
+++ main.js
@@ -1,11 +1,29 @@
-jQuery(function(){
-    jQuery('#loadPage').click(function(){
-        jQuery('#pagecontainer').load('main.html', 
-                function(){alert('Content Successfully Loaded.')} 
-        );
+const nav = document.querySelector('.bar')
+fetch('bar.html')
+.then(res=>res.text())
+.then(data=>{
+    nav.innerHTML=data
+})
+
+var datas = [
+    { id: 1, html:"articles/1.html", title:"從零開始的網誌開張啦！",
+        info: "還在定位這個網誌的目標中，目前有學習筆記，資料分享..." }
+];
+
+$(document).ready(function(){
+    //alert('Successfully Loaded. Currently ' + datas.length + " articles.")
+    //jQuery('#bar').load('bar.html');    
+
+    datas.forEach(function(value){
+      addNewArticle(value);
     });
 })
 
-$(document).ready(function(){
-    //$('#bar').load('main.html', function(){alert('Content Successfully Loaded.')});
-})
+function addNewArticle(data){
+    document.getElementById("items").innerHTML += 
+    "<div class=\"item\">" +
+        "<h3>" + data.title + "</h3>" +
+        "<h4>" + data.info + "</h4>" +
+        "<div><a href=\"" + data.html + "\">more...</a></div>" +
+    "</div>";
+}
